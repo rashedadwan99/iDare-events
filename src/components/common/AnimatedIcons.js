d6ad8e0d6ee564f99ed1fdd7d1a5630ce51b7a21
rashedadwan99 <rashedadwan56789@gmail.com>
2023-09-18 11:29:38 +0300

    dkofdnfhbgjk

--- src/components/common/AnimatedIcons.js
+++ src/components/common/AnimatedIcons.js
@@ -1,16 +1,14 @@
 import React from "react";
-import { BsFillSquareFill, BsFillCircleFill } from "react-icons/bs";
+import { BsSquare } from "react-icons/bs";
 import { AiOutlineClose } from "react-icons/ai";
 import "../../styles/animated-icons.css";
 function AnimatedIcons() {
   return (
     <>
       <div className="animated-icons__square position">
-        <BsFillSquareFill />
+        <BsSquare />
       </div>
-      <div className="animated-icons__circle position">
-        <BsFillCircleFill />
-      </div>
+
       <div className="animated-icons__close position">
         <AiOutlineClose />
       </div>
