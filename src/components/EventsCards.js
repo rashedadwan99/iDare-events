c464534874c3204b561532b498e919d17da83ca2
rashedadwan99 <rashedadwan56789@gmail.com>
2023-09-25 08:46:41 +0300

    fix forms

--- src/components/EventsCards.js
+++ src/components/EventsCards.js
@@ -7,6 +7,7 @@ import { getImageSrc } from "../services/imageServices";
 import EventTimeLocation from "./EventTimeLocation";
 import { useSelector } from "react-redux";
 import Premium from "./Premium";
+import AosContainer from "./common/Aos";
 function EventsCard({ events }) {
   const navigate = useNavigate();
   const isArabic = useSelector((state) => state.language.isArabic);
@@ -27,38 +28,48 @@ function EventsCard({ events }) {
           onClick={() => handleGoToEventPage(e.id)}
         >
           <Col md={6}>
-            <Image
-              fluid
-              src={getImageSrc(e.events_list_image)}
-              alt="event_list_img"
-              className="event_list_img"
-            />
+            <AosContainer
+              animation_name_scroll_down="zoom_in"
+              animation_name_scroll_up="zoom-out"
+            >
+              <Image
+                fluid
+                src={getImageSrc(e.events_list_image)}
+                alt="event_list_img"
+                className="event_list_img"
+              />
+            </AosContainer>
           </Col>
           <Col md={6}>
-            <Row className="justify-content-between align-items-center event-name mt-2 mb-2">
-              <Col xs={8} sm={8}>
-                <h6 style={infoStyle}>{isArabic ? e.name_ar : e.name}</h6>
-              </Col>
-              <Col xs={4} sm={4}>
-                {e.is_premium ? <Premium event={e} /> : <></>}
-              </Col>
-            </Row>
-            <Row className="mb-3">
-              <Col>
-                <p style={infoStyle} className="event_desc">
-                  {isArabic ? e.short_description_ar : e.short_description}
-                </p>
-              </Col>
-            </Row>
-
-            <Col md={12}>
-              <Row
-                className="justify-content-start"
-                style={{ fontWeight: "bold", fontSize: "12px" }}
-              >
-                <EventTimeLocation event={e} />
+            <AosContainer
+              animation_name_scroll_down="zoom_in"
+              animation_name_scroll_up="zoom-out"
+            >
+              <Row className="justify-content-between align-items-center event-name mt-2 mb-2">
+                <Col xs={8} sm={8}>
+                  <h6 style={infoStyle}>{isArabic ? e.name_ar : e.name}</h6>
+                </Col>
+                <Col xs={4} sm={4}>
+                  {e.is_premium ? <Premium event={e} /> : <></>}
+                </Col>
               </Row>
-            </Col>
+              <Row className="mb-3">
+                <Col>
+                  <p style={infoStyle} className="event_desc">
+                    {isArabic ? e.short_description_ar : e.short_description}
+                  </p>
+                </Col>
+              </Row>
+
+              <Col md={12}>
+                <Row
+                  className="justify-content-start"
+                  style={{ fontWeight: "bold", fontSize: "12px" }}
+                >
+                  <EventTimeLocation event={e} />
+                </Row>
+              </Col>
+            </AosContainer>
           </Col>
         </Row>
       );
