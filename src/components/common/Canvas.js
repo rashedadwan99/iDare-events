6384bf79795466c2b4a47728983ea0e805183b96
rashedadwan99 <rashedadwan56789@gmail.com>
2023-09-28 01:25:03 +0300

    fix protected routes

--- src/components/common/Canvas.js
+++ src/components/common/Canvas.js
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { useLocation, useNavigate } from "react-router-dom";
 import { Col, Row } from "react-bootstrap";
 import ClickOutsideAlerter from "./ClickOutSideAlerter";
 import CanvasHeader from "../CanvasHeader";
 import CanvasBody from "../CanvasBody";
 import { closeCanvasAction } from "../../redux/actions/canvasActions";
 import "../../styles/canvas.css";
-import { useEffect } from "react";
 function Canvas() {
   const show = useSelector((state) => state.canvas.show);
+  const location = useLocation();
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   useEffect(() => {
     if (show) {
+      if (window.innerWidth >= 1150) {
+      }
       const handleBackButton = (e) => {
+        navigate(location.pathname);
         dispatch(closeCanvasAction());
       };
 
@@ -21,16 +26,23 @@ function Canvas() {
           dispatch(closeCanvasAction());
         }
       };
+      const handleResize = () => {
+        if (window.innerWidth >= 1150) {
+          dispatch(closeCanvasAction());
+        }
+      };
 
+      window.addEventListener("resize", handleResize);
       window.addEventListener("popstate", handleBackButton);
       window.addEventListener("keydown", handleEscapeKey);
 
       return () => {
         window.removeEventListener("popstate", handleBackButton);
         window.removeEventListener("keydown", handleEscapeKey);
+        window.removeEventListener("resize", handleResize);
       };
     }
-  }, [show]);
+  }, [show, window.innerWidth]);
   return (
     <>
       <ClickOutsideAlerter onOutsideClick={() => dispatch(closeCanvasAction())}>
