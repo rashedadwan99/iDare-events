ef7d07b0330ed139040b83272859b33442798d02
rashedadwan99 <rashedadwan56789@gmail.com>
2024-01-17 18:57:12 +0300

    sdsd

--- src/components/GalleryVideos.js
+++ src/components/GalleryVideos.js
@@ -1,16 +1,9 @@
 import { useSelector } from "react-redux";
-import { Col } from "react-bootstrap";
-import DomParser from "./common/DomParser";
 
 function GalleryVideos() {
   const videos = useSelector((state) => state.events.eventMedia.videos);
-  return videos.map((v) => {
-    return (
-      <Col key={v.id} xs={11} sm={11} md={8} lg={5}>
-        <DomParser htmlResponse={v.embed_code} />
-      </Col>
-    );
-  });
+  console.log(videos);
+  return videos.map((v) => v);
 }
 
 export default GalleryVideos;
--- src/pages/EventPage.js
+++ src/pages/EventPage.js
@@ -9,6 +9,8 @@ import { fontCode, fontLink } from "../styles/eventStyles";
 import { useDispatch } from "react-redux";
 import { GetEventMedia } from "../redux/actions/eventActions";
 import { sortData } from "../components/utils/sort";
+import DomParser from "../components/common/DomParser";
+import { Col } from "react-bootstrap";
 const EventPage = memo(function () {
   const { id } = useParams();
   const allEvents = useSelector((state) => state.events.allEvents);
@@ -20,13 +22,17 @@ const EventPage = memo(function () {
     if (event) {
       const style = document.createElement("style");
       const eventFontLink = event.google_fonts_link ?? fontLink;
-      style.innerHTML = `.event-page, .event-header{ ${
-        event.google_font_code ?? fontCode
-      } }`;
+
       dispatch(
         GetEventMedia({
           images: sortedImages,
-          videos: sortedVideos,
+          videos: sortedVideos.map((v) => {
+            return (
+              <Col key={v.id} xs={11} sm={11} md={8} lg={5}>
+                <DomParser htmlResponse={v.embed_code} />
+              </Col>
+            );
+          }),
         })
       );
 
@@ -36,6 +42,9 @@ const EventPage = memo(function () {
       if (eventFontLink) {
         document.head.appendChild(container);
         document.head.appendChild(style);
+        window.removeEventListener("touchstart", {
+          passive: true,
+        });
       }
 
       return () => {
