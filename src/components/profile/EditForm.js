1bca4f2cdd93043c8dd1492d01ad2639d8535a2c
rashedadwan99 <rashedadwan56789@gmail.com>
2024-05-09 18:37:01 +0300

    hi

--- src/components/profile/EditForm.js
+++ src/components/profile/EditForm.js
@@ -11,6 +11,8 @@ import { genderOptions } from "../data/genders";
 import CommonButton from "../common/Button";
 import { setToken, updateProfile } from "../../services/userService";
 import { getProfileAction } from "../../redux/actions/userActions";
+import { useNavigate } from "react-router-dom";
+import { profileRoute } from "../../routes";
 
 function EditForm() {
   const { isArabic } = useSelector((state) => state.language);
@@ -44,6 +46,11 @@ function EditForm() {
   }, []);
   const { t } = useTranslation();
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleNavigate = () => {
+    navigate(profileRoute);
+    window.scrollTo(0, 0);
+  };
   const handleSave = async () => {
     let isValidPass = true;
     const {
@@ -81,6 +88,7 @@ function EditForm() {
             dispatch(getProfileAction());
             Toast("info", t("profile-updated"));
             setIsSending(false);
+            handleNavigate();
           } else {
             Toast("error", t("error_occoured"));
             setIsSending(false);
@@ -92,114 +100,132 @@ function EditForm() {
       }
     }
   };
-  return (
-    <Row className="justify-content-center my-4">
-      <Col xs={11} sm={10} md={6} lg={4}>
-        <Form className="edit-form">
-          <Row className="justify-content-center my-2">
-            <Col xs={11} sm={11}>
-              <FormElement
-                data={data}
-                setData={setData}
-                value={data.name}
-                name="name"
-                label={t("name")}
-              />
-            </Col>
-          </Row>
 
-          <Row className="justify-content-center my-2">
-            <Col xs={11} sm={11}>
-              <FormElement
-                data={data}
-                setData={setData}
-                value={data.phone}
-                name="phone"
-                label={t("phone")}
-              />
-            </Col>
-          </Row>
-          <Row className="justify-content-center my-2">
-            <Col xs={11} sm={11}>
-              <FormElement
-                data={data}
-                setData={setData}
-                value={data.national_number}
-                name="national_number"
-                label={t("national_number")}
-              />
-            </Col>
-          </Row>
-          <Row className="justify-content-center">
-            <Col xs={11} sm={11} md={5}>
-              <FormElement
-                name="country_id"
-                defaultOption={
-                  isArabic ? value.country.name_ar : value.country.name
-                }
-                options={Countries}
-                data={data}
-                setData={setData}
-                path={isArabic ? "name_ar" : "name"}
-                element="select"
-                label={t("Country")}
-              />
-            </Col>
-            <Col xs={11} sm={11} md={5}>
-              <FormElement
-                name="city_id"
-                defaultOption={isArabic ? value.city.name_ar : value.city.name}
-                options={cities}
-                data={data}
-                setData={setData}
-                path={isArabic ? "name_ar" : "name"}
-                element="select"
-                label={t("City")}
-              />
-            </Col>
-          </Row>
-          <Row className="justify-content-center">
-            <Col xs={11} sm={11} md={10}>
-              <FormElement
-                defaultOption={
-                  isArabic ? value.gender.name_ar : value.gender.name
-                }
-                label={t("gender")}
-                name="gender_id"
-                options={genderOptions(t)}
-                data={data}
-                setData={setData}
-                path="name"
-                element="select"
-              />
-            </Col>
-          </Row>
-          <Row className="justify-content-center mb-3">
-            <Col xs={11} sm={11} md={10}>
-              <FormElement
-                data={data}
-                setData={setData}
-                value={data.password}
-                name="password"
-                type="password"
-                label={t("password-change")}
-                placeholder={t("password")}
-              />
-            </Col>
-          </Row>
-          <Row className="justify-content-center mb-3">
-            <Col xs={11} sm={10}>
-              <CommonButton
-                element="button"
-                label={t("save")}
-                onClick={handleSave}
-                disabled={isSending}
-              />
-            </Col>
-          </Row>
-        </Form>
-      </Col>
-    </Row>
+  return (
+    <>
+      <Row className="justify-content-center mt-4">
+        <h3 className="sec-title">{t("profile-edit")}</h3>
+      </Row>
+      <Row className="justify-content-center my-4">
+        <Col xs={11} sm={10} md={6} lg={4}>
+          <Form className="edit-form">
+            <Row className="justify-content-center my-2">
+              <Col xs={11} sm={11}>
+                <FormElement
+                  data={data}
+                  setData={setData}
+                  value={data.name}
+                  name="name"
+                  label={t("name")}
+                />
+              </Col>
+            </Row>
+
+            <Row className="justify-content-center my-2">
+              <Col xs={11} sm={11}>
+                <FormElement
+                  data={data}
+                  setData={setData}
+                  value={data.phone}
+                  name="phone"
+                  label={t("phone")}
+                />
+              </Col>
+            </Row>
+            <Row className="justify-content-center my-2">
+              <Col xs={11} sm={11}>
+                <FormElement
+                  data={data}
+                  setData={setData}
+                  value={data.national_number}
+                  name="national_number"
+                  label={t("national_number")}
+                />
+              </Col>
+            </Row>
+            <Row className="justify-content-center">
+              <Col xs={11} sm={11} md={5}>
+                <FormElement
+                  name="country_id"
+                  defaultOption={
+                    isArabic ? value.country.name_ar : value.country.name
+                  }
+                  options={Countries}
+                  data={data}
+                  setData={setData}
+                  path={isArabic ? "name_ar" : "name"}
+                  element="select"
+                  label={t("Country")}
+                />
+              </Col>
+              <Col xs={11} sm={11} md={5}>
+                <FormElement
+                  name="city_id"
+                  defaultOption={
+                    isArabic ? value.city.name_ar : value.city.name
+                  }
+                  options={cities}
+                  data={data}
+                  setData={setData}
+                  path={isArabic ? "name_ar" : "name"}
+                  element="select"
+                  label={t("City")}
+                />
+              </Col>
+            </Row>
+            <Row className="justify-content-center">
+              <Col xs={11} sm={11} md={10}>
+                <FormElement
+                  defaultOption={
+                    isArabic ? value.gender.name_ar : value.gender.name
+                  }
+                  label={t("gender")}
+                  name="gender_id"
+                  options={genderOptions(t)}
+                  data={data}
+                  setData={setData}
+                  path="name"
+                  element="select"
+                />
+              </Col>
+            </Row>
+            <Row className="justify-content-center mb-3">
+              <Col xs={11} sm={11} md={10}>
+                <FormElement
+                  data={data}
+                  setData={setData}
+                  value={data.password}
+                  name="password"
+                  type="password"
+                  label={t("password-change")}
+                  placeholder={t("password")}
+                />
+              </Col>
+            </Row>
+            <Row className="justify-content-center mb-3">
+              <Col xs={11} sm={10}>
+                <CommonButton
+                  element="button"
+                  label={t("save")}
+                  onClick={handleSave}
+                  disabled={isSending}
+                />
+              </Col>
+            </Row>
+            <Row className="justify-content-center mb-3">
+              <Col xs={11} sm={10}>
+                <CommonButton
+                  element="button"
+                  label={t("back")}
+                  variant="danger"
+                  onClick={handleNavigate}
+                />
+              </Col>
+            </Row>
+          </Form>
+        </Col>
+      </Row>
+    </>
   );
 }
 
