52b795d05cf94f3c1a8681b418f3dc3dac1f9fa6
rashedadwan99 <rashedadwan56789@gmail.com>
2024-05-09 20:56:38 +0300

    hi

--- src/components/profile/PImage.js
+++ src/components/profile/PImage.js
@@ -1,13 +1,18 @@
 import React from "react";
-import { Col, Row } from "react-bootstrap";
+import { Col, Image, Row } from "react-bootstrap";
 import { FaCamera } from "react-icons/fa6";
 import Input from "../common/Input";
+import User from "../../images/user.png";
+import { useSelector } from "react-redux";
 
 function PImage() {
+  const { value } = useSelector((state) => state.user);
   return (
     <Row className="my-3">
       <Col>
         <div className="pimage-container">
+          <Image src={User} />
+
           <div className="pimage-picker">
             <FaCamera />
             <input type="file" accept="image/*" />
